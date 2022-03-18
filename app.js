f2864574e02242d28751081f33ad1eeb94cb4ec6
tajudeenazeez <azeezproto@gmail.com>
2022-03-18 03:32:40 +0100

    create login for new user, save the session in localstorage to remember the username

---
+++ app.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input")
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME = "username"
+
+function loginHandler(event) {
+    event.preventDefault();
+    const username = loginInput.value
+    callUsername(username)
+    localStorage.setItem(USERNAME, username)   
+}
+
+function callUsername(username) {
+    greeting.textContent = `HELLO ${username}`
+    loginForm.classList.toggle(HIDDEN_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME)
+    if(savedUsername === null) {
+        loginForm.classList.remove(HIDDEN_CLASSNAME)
+        loginForm.addEventListener("submit", loginHandler)
+    }  else{
+        callUsername(savedUsername)
+    } 
